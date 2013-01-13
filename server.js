08346bc49ec735bc81f707fa96c74b56943adb70
ChetanD <chetandhembre@gmail.com>
2013-01-13 18:28:12 +0530

    init

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require ("express");
 var server = express();
 var fs = require("fs");
-server.listen(3000);
+
+var port = process.env.PORT || 5000;
+
+server.listen(port);
 
 
 server.use(express.static(__dirname ));
