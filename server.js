8e42d24084ea15d3975c38a2377c0edca0bd54be
ChetanD <chetandhembre@gmail.com>
2013-01-13 18:45:50 +0530

    init

--- server.js
+++ server.js
@@ -9,37 +9,4 @@ server.listen(port);
 
 server.use(express.static(__dirname ));
 
-server.get("/data.json",function(req,res){
-
-fs.readFile("./data.json",function(err,data){
-	console.log(data);
-    if(err) {
-    	res.writeHead(200);
-    	res.end("Error in loading data");
-    } else {
-    	res.writeHead(200,{
-    		"Content-Type":"json",
-
-    	});
-    	res.end(data);
-    }
-});
-});
-
-
-server.get("/info.json",function(req,res){
-
-fs.readFile("./info.json",function(err,data){
-	console.log(data);
-    if(err) {
-    	res.writeHead(200);
-    	res.end("Error in loading data");
-    } else {
-    	res.writeHead(200,{
-    		"Content-Type":"json",
-
-    	});
-    	res.end(data);
-    }
-});
-});
+ 
